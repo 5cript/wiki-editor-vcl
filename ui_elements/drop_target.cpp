31ca7cf53b24e541d26557f89ff8a10557c97cd2
5cript <tim06tr@gmail.com>
2016-09-30 17:25:33 +0200

    Switched system over to a layout system supported by TGridPanels. This will reduce the chaos and therefore make the code less prone.

---
+++ ui_elements/drop_target.cpp
@@ -0,0 +1,34 @@
+//---------------------------------------------------------------------------
+
+#pragma hdrstop
+
+#include "drop_target.h"
+//---------------------------------------------------------------------------
+namespace WikiElements
+{
+//---------------------------------------------------------------------------
+	DropTarget::DropTarget(Section* parentSection)
+		: Element{parentSection}
+	{
+		auto* parent = parentSection->getLayout()->getControl();
+
+		// Style
+		control_->ParentBackground = false;
+		control_->Color = clLime;
+		control_->BorderStyle = bsNone;
+		control_->BevelOuter = bvRaised;
+
+		// Positioning and Size
+		control_->Height = 10;
+		//control_->Top = parentSection->getMostBottom() + sectionSplitPadding;
+		//control_->Left = leftSectionPadding;
+		//control_->Width = parent->Width - leftSectionPadding - rightSectionPadding;
+	}
+//---------------------------------------------------------------------------
+	void DropTarget::styleChanged(WretchedCss::StyleSheet const& style, StyleParser const& parser)
+	{
+
+	}
+//---------------------------------------------------------------------------
+}
+#pragma package(smart_init)
